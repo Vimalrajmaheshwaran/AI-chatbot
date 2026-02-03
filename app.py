4e82f1e8995751668fe467be81fac2a9a556def4
Vimalraj M <vimalrajmahesh3004@gmail.com>
2026-02-03 20:59:57 +0530

    Removed sensitive API key

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # --- CONFIGURATION ---
 # GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-GOOGLE_API_KEY = "AIzaSyCGrr4F8amrqeIJsnm6fTVSw2SPoZeAIMk" # User must provide their own key
+GOOGLE_API_KEY = "Enter your key here" # User must provide their own key
 # ---------------------
 
 client = genai.Client(api_key=GOOGLE_API_KEY)
@@ -16,7 +16,18 @@ def home():
     return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
+@app.route('/chat', methods=['POST'])
 def chat():
     try:
         user_data = request.get_json()
         user_message = user_data.get('message')
+
+        response = client.models.generate_content(
+            model='gemini-flash-latest', 
+            contents=user_message
+        )
+        
+        return jsonify({"response": response.text})
+
+    except Exception as e:
+        return jsonify({"response": f"System Error: {str(e)}"})
